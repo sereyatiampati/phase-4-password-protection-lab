fe538b88633039a8a49d164acb141b64c381fdda
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-23 08:30:42 +0300

    done

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
+  
 end
 
 group :test do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # before_action :authorize
+  # def auhthorize
+  #   return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? user_id
+  # end
 
 end
